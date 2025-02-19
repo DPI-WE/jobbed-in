e6f3da5d3e2da8776c0793c77e9fa52b450f6dd0
Ian Heraty <heratyian@gmail.com>
2025-02-19 14:03:10 -0600

    Add pagination with kaminari gem to jobs index. Also added scopes to job model.

--- app/models/job.rb
+++ app/models/job.rb
@@ -15,4 +15,8 @@
 class Job < ApplicationRecord
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
   # belongs_to :category, required: true, class_name: "Category", foreign_key: "category_id"
+  #
+
+  scope :default_order, -> { order(deadline: :asc) }
+  scope :upcoming, -> { where(deadline: Date.today..) }
 end
